ba832895d0233938ac873d9858ce473cbc635cc5
humanfriend22 <humanfriend22@gmail.com>
2024-03-25 22:28:27 -0700

    added spam protection

--- main.py
+++ main.py
@@ -1,22 +1,55 @@
+import time
+from threading import Timer
+
 from inputs import devices, get_key
 from picamera2 import Picamera2, Preview
 
-print(devices.keyboards);
+print(devices.keyboards)
 
-picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL, x=100, y=100) 
-picam2.configure(picam2.create_preview_configuration())
-picam2.set_controls({"FrameDurationLimits": (33333, 33333)})
+# Camera
+camera = Picamera2()
+camera.start_preview(Preview.QTGL, x=100, y=100) 
+camera.configure(camera.create_preview_configuration())
+camera.set_controls({"FrameDurationLimits": (33333, 33333)})
+camera.start()
 
-picam2.start()
+# Handle Keyboard Input
+def debounce(func, delay):
+    def debounced(*args, **kwargs):
+        debounced.last_call = time.time()
+
+        def call_it():
+            if time.time() - debounced.last_call >= delay:
+                func(*args, **kwargs)
+
+        if hasattr(debounced, 'timer'):
+            debounced.timer.cancel()
+        debounced.timer = Timer(delay, call_it)
+        debounced.timer.start()
+
+    return debounced
+
+def update_motors():
+    print(state)
+
+handle_keyboard = debounce(update_motors, 0.1)
 running = True
 state = {}
 while running:
-    event = get_key()[0]
+    events = get_key()
+    event = events[0]
+
+    # For debug only just in case this is ever more than 1
+    if len(events) > 1:
+        print('Multiple events: ', events)
+
     if event.ev_type == 'Key':
         if event.code == 'KEY_0':
             running = False
-        state[event.code] = event.state
-        print(state)
+            continue
 
-picam2.stop()
+        if event.code in ['KEY_W', 'KEY_A', 'KEY_S', 'KEY_D', 'KEY_Q', 'KEY_E', 'KEY_F']:
+            state[event.code] = event.state
+            handle_keyboard()
+
+camera.stop()
